e73f08f5270fe14186313fa39a454309797a5d03
Morgan Smith <morgansmith0219@gmail.com>
2019-08-01 22:36:12 +0000

    added ones on line 40

--- blur2.rb
+++ blur2.rb
@@ -2,8 +2,8 @@
 class Image
   attr_accessor :pixels
 
-  def initialize(pixels)
-    @pixels = pixels
+  def initialize(array)
+    @pixels = array
   end
 
   def output_image
@@ -12,7 +12,6 @@ class Image
     end
   end
 
-
   def blur(distance)
     distance.times do
       ones = find_ones
@@ -20,9 +19,9 @@ class Image
         row.each_with_index do |val, j|
           if ones.include?([i, j])
             @pixels[i-1][j] = 1 unless i == 0
-            @pixels[i+1][j] = 1 unless i == (@pixels - 1)
+            @pixels[i+1][j] = 1 unless i == (@pixels.length - 1)
             @pixels[i][j-1] = 1 unless j == 0
-            @pixels[i][j+1] = 1 unless j == (@row - 1)
+            @pixels[i][j+1] = 1 unless j == (row.length - 1)
           end
         end
       end
@@ -38,22 +37,23 @@ class Image
         end
       end
     end
+    ones
   end
 end
 
-array = [
+image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 0, 1],
   [0, 0, 0, 0]
-]
-image = Image.new(array)
+])
+
 image.output_image
-puts "--"
+puts "---"
 puts "blurred image"
 # enter blur distance
-image.blur(3)
-puts "--"
-image.find_ones
+image.blur(1)
+puts "---"
 image.output_image
-puts "--"
+puts "---"
+# puts image.get_ones
